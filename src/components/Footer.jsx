2eba442c4a1d50aa4fe78b2ad820ccd0e85df14f
Azzedde <ja_aitsaid@esi.dz>
2023-05-01 15:23:11 +0100

    Slight Modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ import {
   FaTwitterSquare,
   FaYoutubeSquare,
 } from 'react-icons/fa';
+import {HashLink as Link} from 'react-router-hash-link'
 
 const Footer = () => {
   return (
@@ -16,9 +17,9 @@ const Footer = () => {
         <p className='py-4 text-3xl font-bold text-center'>Follow Us</p>
         <div className='flex justify-between md:w-[75%] my-6 ml-5 '>
             <FaFacebookSquare size={30} />
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
-            <FaYoutubeSquare size={30} />
+           <Link to='https://www.instagram.com/zer0.w4ste/'><FaInstagram size={30} /></Link> 
+           <Link to='https://twitter.com/Zer0W4ste0' > <FaTwitterSquare size={30} /></Link>
+           <Link to='https://www.youtube.com/channel/UCZmqHlJYmgs4wjBpWW1bcRg'> <FaYoutubeSquare size={30} /> </Link>
         </div>
       </div>
 
